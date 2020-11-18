c6857bdf8bd26ffab57d94b2a24053f73389c248
77wiz77 <arturkim30@gmail.com>
2020-11-18 18:33:08 +1100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <meta charset="utf-8">
 </head>
 <body>
-<h2>ПРИВЕТ и пока:</h2>
+<h2>ПРИВЕТ и пока b cyjdf:</h2>
 <form action="display.php" method="POST">
 <p>Введите имя: <input type="text" name="firstname" /></p>
 <p>Введите фамилию: <input type="text" name="lastname" /></p>
