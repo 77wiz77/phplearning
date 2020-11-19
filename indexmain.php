be934188137e959276d75561d77107457812409a
77wiz77 <arturkim30@gmail.com>
2020-11-19 18:26:32 +1100

    1

---
+++ indexmain.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MAIN</title>
+</head>
+<body>
+  <h1>Главная страница</h1>
+  <ul>
+    <li><a href="/legacy.php"><h2>1 Основы</h2></a></li>
+    <li><a href="/legacy.php"><h2>2 Передача параметров, работа с формами</h2></a></li>
+    <li><a href="/legacy.php"><h2>3 Базовые возможности php</h2></a></li>
+    <li><a href="/legacy.php"><h2>4 Работа с файловой системой</h2></a></li>
+    <li><a href="/legacy.php"><h2>5 ООП</h2></a></li>
+    <li><a href="/legacy.php"><h2>6 Работа с базой данных MySQL</h2></a></li>
+  </ul>
+  
+</body>
+</html>
