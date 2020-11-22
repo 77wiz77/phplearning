ac8df378ac386dd6f300d39f46c8c2c54014dfd3
77wiz77 <arturkim30@gmail.com>
2020-11-22 18:08:11 +1100

    1

---
+++ forForm/form.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MAIN</title>
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+  <header class="header-page">
+    <h1>Главная страница</h1>
+  </header>
+
+  <main>
+    <section>
+        <ul>
+          <li><a href="/forLegacy/legacy.php"><h2>Privet form</h2></a></li>
+        </ul>
+    </section>
+    
+  </main>
+
+  <footer class="footer-page">
+    <p></p>
+  </footer>
+  
+  <!--<script src="js/.js"></script>-->
+</body>
+</html>
--- index.php
+++ index.php
@@ -14,8 +14,8 @@
   <main>
     <section>
         <ul>
-          <li><a href="./phplearning/forForm/index.php"><h2>1 Основы</h2></a></li>
-          <li><a href="forForm/index.php"><h2>2 Передача параметров, работа с формами</h2></a></li>
+          <li><a href="/forLegacy"><h2>1 Основы</h2></a></li>
+          <li><a href="forForm/form.php"><h2>2 Передача параметров, работа с формами</h2></a></li>
           <li><a href="/test.php"><h2>3 Базовые возможности php</h2></a></li>
           <li><a href="/legacy.php"><h2>4 Работа с файловой системой</h2></a></li>
           <li><a href="/legacy.php"><h2>5 ООП</h2></a></li>
