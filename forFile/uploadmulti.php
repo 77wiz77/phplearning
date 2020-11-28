11d16c70ec29e629e7eb2294593b52a13d1fb8b3
77wiz77 <arturkim30@gmail.com>
2020-11-29 01:00:28 +1100

    1

--- forFile/uploadmulti.php
+++ forFile/uploadmulti.php
@@ -7,11 +7,12 @@
   <?php
   if($_FILES)
   {
-      foreach ($_FILES["uploads"]["error"] as $key => $error) {
-          if ($error == UPLOAD_ERR_OK) {
-              $tmp_name = $_FILES["uploads"]["tmp_name"][$key];
-              $name = $_FILES["uploads"]["name"][$key];
-              move_uploaded_file($tmp_name, "$name");
+      foreach ($_FILES["uploads"]["error"] as $key => $error) { //извлечение ключей элементов с помощью цикла
+        //иначе говоря каждый файл отдельно проверяется. Извлекается из массива $_FILES и помещается в переменную error
+          if ($error == UPLOAD_ERR_OK) { //если файл существует
+              $tmp_name = $_FILES["uploads"]["tmp_name"][$key]; //путь к временному месту файла (ну и его имя)
+              $name = $_FILES["uploads"]["name"][$key]; //путь к постоянному месту файла (ну и его имя)
+              move_uploaded_file($tmp_name, "$name"); //первое это временный путь, второе постоянный
           }
       }
   }
